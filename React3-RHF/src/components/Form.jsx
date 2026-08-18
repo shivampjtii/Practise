637e43d92a08246c9c0c4aec2ff01e4029c91954
shivampjtii <shivampjtii@gmail.com>
2026-08-18 14:44:18 +0530

    using object

--- React3-RHF/src/components/Form.jsx
+++ React3-RHF/src/components/Form.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) =>{
+    const { name, value } = e.target;
+    setFormData((prev)=>({...prev, [name]: value}));
+  }
+
   return (
     <div className='flex p-4'>
         <form action="" className='flex flex-col gap-4'>
-            <input className='border rounded p-2' type="text" placeholder='Name' />
-            <input className='border rounded p-2' type="email" placeholder='Email' />
-            <input className='border rounded p-2' type="password" placeholder='Password' />
+            <input name='name' value={formData.name} onChange={handleChange} className='border rounded p-2' type="text" placeholder='Name' />
+            <input name='email' value={formData.email} onChange={handleChange} className='border rounded p-2' type="email" placeholder='Email' />
+            <input name='password' value={formData.password} onChange={handleChange} className='border rounded p-2' type="password" placeholder='Password' />
             <button className='border rounded p-2 cursor-pointer' type="submit">Submit</button>
         </form>
     </div>
