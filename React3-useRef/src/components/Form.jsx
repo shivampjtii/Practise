556a363588f4765c92e9649227a48e6a0067d242
shivampjtii <shivampjtii@gmail.com>
2026-08-19 21:39:09 +0530

    project

--- React3-useRef/src/components/Form.jsx
+++ React3-useRef/src/components/Form.jsx
@@ -1,24 +1,47 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from "react";
 
 const Form = () => {
-
   const [formData, setFormData] = useState({});
+  const formRef = useRef({});
+  console.log(formData);
 
-  const handleChange = (e) =>{
-    const { name, value } = e.target;
-    setFormData((prev)=>({...prev, [name]: value}));
-  }
+  const handleForm = (e) => {
+    e.preventDefault();
+    const obj = {
+      name: formRef.current.name.value,
+      email: formRef.current.email.value,
+      password: formRef.current.password.value,
+    };
+    setFormData(obj);
+  };
 
   return (
-    <div className='flex p-4'>
-        <form action="" className='flex flex-col gap-4'>
-            <input name='name' value={formData.name} onChange={handleChange} className='border rounded p-2' type="text" placeholder='Name' />
-            <input name='email' value={formData.email} onChange={handleChange} className='border rounded p-2' type="email" placeholder='Email' />
-            <input name='password' value={formData.password} onChange={handleChange} className='border rounded p-2' type="password" placeholder='Password' />
-            <button className='border rounded p-2 cursor-pointer' type="submit">Submit</button>
-        </form>
+    <div className="flex p-4">
+      <form onSubmit={handleForm} className="flex flex-col gap-4">
+        <input
+          ref={(e) => (formRef.current.name = e)}
+          className="border rounded p-2"
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          ref={(e) => (formRef.current.email = e)}
+          className="border rounded p-2"
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          ref={(e) => (formRef.current.password = e)}
+          className="border rounded p-2"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="border rounded p-2 cursor-pointer" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
